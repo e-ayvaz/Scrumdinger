099dc52c62688fae13fac67c320a07cb442e71b5
Enes Ayvaz <enes.ayvaz@mobven.com>
2024-01-06 16:01:09 +0300

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Enes Ayvaz on 6.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
