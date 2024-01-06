8fb7a0949eb354a60d30da9eb94e2914772bc761
Enes Ayvaz <enes.ayvaz@mobven.com>
2024-01-06 19:12:16 +0300

    added navigation link to detailview and attendees section

--- Scrumdinger/Views/DetailView.swift
+++ Scrumdinger/Views/DetailView.swift
@@ -13,9 +13,11 @@ struct DetailView: View {
     var body: some View {
         List {
             Section(header: Text("Meeting Info")) {
-                Label("Start Meeting", systemImage: "timer")
-                    .font(.headline)
-                    .foregroundColor(.accentColor)
+                NavigationLink(destination: MeetingView()) {
+                    Label("Start Meeting", systemImage: "timer")
+                        .font(.headline)
+                        .foregroundColor(.accentColor)
+                }
                 HStack {
                     Label("Length", systemImage: "clock")
                     Spacer()
@@ -31,11 +33,19 @@ struct DetailView: View {
                         .background(scrum.theme.mainColor)
                         .cornerRadius(4)
                 }
+                .accessibilityElement(children: .combine)
             }
-        }
+            Section(header: Text("Attendees")) {
+                ForEach(scrum.attendees) { attendee in
+                    Label(attendee.name, systemImage: "person")
+                }
+            }
+        }.navigationTitle(scrum.title)
     }
 }
 
 #Preview {
-    DetailView(scrum: DailyScrum.sampleData[0])
+    NavigationStack {
+        DetailView(scrum: DailyScrum.sampleData[0])
+    }
 }
