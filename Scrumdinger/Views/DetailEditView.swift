4a43e40cdc2990cbb343772b66275865b8e8418e
Enes Ayvaz <enes.ayvaz@mobven.com>
2024-01-07 23:57:29 +0300

    Add SliderView in detaileditView and add lengthInMinutesAsDouble variable for slider work with double variables.

--- Scrumdinger/Views/DetailEditView.swift
+++ Scrumdinger/Views/DetailEditView.swift
@@ -10,11 +10,16 @@ import SwiftUI
 struct DetailEditView: View {
     
     @State private var scrum = DailyScrum.emptyScrum
-
+    
     var body: some View {
         Form {
             Section(header:  Text("Meeting Info")) {
                 TextField("Title",text: $scrum.title)
+                HStack {
+                    Slider(value: $scrum.lengthInMinutesAsDouble, in: 5...30, step: 1) {
+                        Text("Length")
+                    }
+                }
                 
             }
         }
